e211076b795ba413e4569005ca3c75ce21e31551
Fahim Al Jadid <b170606023@pharm.jnu.ac.bd>
2023-07-05 12:53:19 +0600

    implemented the role-based operations in AuthService using UserRepository

--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -1,4 +1,33 @@
 import { Injectable } from '@nestjs/common';
+import { UserRepository } from 'src/users/users.repository';
+import { Role } from '../auth/roles';
+import { User } from '../users/user.model';
 
 @Injectable()
-export class AuthService {}
+export class AuthService {
+  constructor(private readonly userRepository: UserRepository) {}
+
+  async assignRole(userId: string, role: Role): Promise<void> {
+    const user = await this.userRepository.findById(userId);
+    if (user) {
+      user.role = role;
+      await this.userRepository.update(user);
+    }
+  }
+
+  async revokeRole(userId: string, role: Role): Promise<void> {
+    const user = await this.userRepository.findById(userId);
+    if (user && user.role === role) {
+      user.role = null;
+      await this.userRepository.update(user);
+    }
+  }
+
+  async updateRole(userId: string, newRole: Role): Promise<void> {
+    const user = await this.userRepository.findById(userId);
+    if (user) {
+      user.role = newRole;
+      await this.userRepository.update(user);
+    }
+  }
+}
